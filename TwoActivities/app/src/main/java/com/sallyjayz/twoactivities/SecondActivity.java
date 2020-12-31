5e4cb8f3975ed2ac8f7e69ebf9c6691f9e22a8d2
sallyjayz <salama.jatau@yahoo.com>
2020-12-31 14:46:29 +0100

    two activities explicit intent

---
+++ TwoActivities/app/src/main/java/com/sallyjayz/twoactivities/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.sallyjayz.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "com.sallyjayz.twoactivities.extra.REPLY";
+
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        /*Get data from main activity*/
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+
+        mReply = findViewById(R.id.editText_second);
+
+    }
+
+    public void returnReply(View view) {
+        /*send data to main activity*/
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+
+    }
+}
