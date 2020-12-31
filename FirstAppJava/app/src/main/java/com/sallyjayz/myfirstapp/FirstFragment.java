180481b827e21778871cfaab31bd5944b1f583e1
sallyjayz <salama.jatau@yahoo.com>
2020-12-31 14:17:44 +0100

    FirstApp

---
+++ FirstAppJava/app/src/main/java/com/sallyjayz/myfirstapp/FirstFragment.java
@@ -0,0 +1,68 @@
+package com.sallyjayz.myfirstapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FirstFragment extends Fragment {
+
+    TextView showCountTextView;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
+        showCountTextView = fragmentFirstLayout.findViewById(R.id.textview_first);
+
+        return fragmentFirstLayout;
+    }
+
+    public void onViewCreated(@NonNull final View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.random_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                int currentCount = Integer.parseInt(showCountTextView.getText().toString());
+                FirstFragmentDirections.ActionFirstFragmentToSecondFragment action =
+                        FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount);
+                NavHostFragment.findNavController(FirstFragment.this).navigate(action);
+
+            }
+        });
+
+        view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast myToast = Toast.makeText(getActivity(), "Hello toast!", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+        view.findViewById(R.id.count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countMe(view);
+            }
+        });
+    }
+
+    private void countMe(View view) {
+        String countString = showCountTextView.getText().toString();
+        Integer count = Integer.parseInt(countString);
+        count++;
+        showCountTextView.setText(count.toString());
+    }
+}
