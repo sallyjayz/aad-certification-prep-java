acad2a8951916cdfd17a519c9ec6633e552e1833
sallyjayz <salama.jatau@yahoo.com>
2021-01-03 13:48:27 +0100

    lifecycle and state

---
+++ TwoActivitiesLifecycleState/app/src/main/java/com/sallyjayz/twoactivities/SecondActivity.java
@@ -0,0 +1,86 @@
+package com.sallyjayz.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = SecondActivity.class.getSimpleName();
+
+    public static final String EXTRA_REPLY = "com.sallyjayz.twoactivities.extra.REPLY";
+
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        /*Get data from main activity*/
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+
+        mReply = findViewById(R.id.editText_second);
+
+        Log.d(TAG, "-------");
+        Log.d(TAG, "onCreate");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    public void returnReply(View view) {
+        /*send data to main activity*/
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+
+        Log.d(TAG, "End SecondActivity");
+
+        finish();
+
+    }
+}
