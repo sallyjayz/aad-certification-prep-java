91989e72c1bb7f681b6267f58224687871f63e28
sallyjayz <salama.jatau@yahoo.com>
2021-01-03 13:59:43 +0100

    implicit intent

---
+++ ImplicitIntents/app/src/main/java/com/sallyjayz/implicitintents/MainActivity.java
@@ -0,0 +1,62 @@
+package com.sallyjayz.implicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText, mLocationEditText, mShareEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edittext);
+        mShareEditText = findViewById(R.id.share_edittext);
+    }
+
+    public void openWebsite(View view) {
+
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+     }
+
+    public void shareText(View view) {
+        String txt = mShareEditText.getText().toString();
+        String mimeType = "text/plain";
+
+        ShareCompat.IntentBuilder.from(this).setType(mimeType)
+                .setChooserTitle(R.string.share_text)
+                .setText(txt)
+                .startChooser();
+    }
+}
